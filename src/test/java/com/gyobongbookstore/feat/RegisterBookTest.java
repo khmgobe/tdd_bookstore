f7fd5893fb78ed3feb124330574003a0bb480bf0
khmgobe <dr94406@naver.com>
2025-02-11 17:05:21 +0900

    3. RegisterBook 레코드 생성 및 밸리데이션 할당.

--- src/test/java/com/gyobongbookstore/feat/RegisterBookTest.java
+++ src/test/java/com/gyobongbookstore/feat/RegisterBookTest.java
@@ -2,6 +2,7 @@ package com.gyobongbookstore.feat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class RegisterBookTest {
 
@@ -10,9 +11,22 @@ class RegisterBookTest {
     @DisplayName("신규 도서를 생성한다.")
     void registerBook()  {
 
-        RegisterBook registerBook = new RegisterBook();
+        final Long id = 1L;
+        final String author = "author";
+        final String title = "title";
+
+        RegisterBook registerBook = new RegisterBook(id, author, title);
     }
 
-    private static class RegisterBook {
+    private record RegisterBook(
+            Long id,
+            String author,
+            String title) {
+
+        RegisterBook {
+            Assert.notNull(id, "아이디는 필수입니다.");
+            Assert.notNull(author, "지은이는 필수입니다.");
+            Assert.notNull(title, "제목은 필수입니다.");
+        }
     }
 }
