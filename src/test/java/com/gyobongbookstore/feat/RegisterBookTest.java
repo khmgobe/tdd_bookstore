a7340de7f4beb950b861f5410a6d416bbb4414a5
khmgobe <dr94406@naver.com>
2025-02-11 16:59:37 +0900

    1. RegisterBookTest 테스트와 테스트케이스를 생성한다.

---
+++ src/test/java/com/gyobongbookstore/feat/RegisterBookTest.java
@@ -0,0 +1,14 @@
+package com.gyobongbookstore.feat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RegisterBookTest {
+
+
+    @Test
+    @DisplayName("신규 도서를 생성한다.")
+    void registerBook()  {
+
+    }
+}
