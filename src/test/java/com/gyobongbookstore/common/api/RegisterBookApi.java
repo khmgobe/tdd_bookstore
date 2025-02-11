22141c4fcf1146f0b0c4ef9ae4137f4e1c85a865
khmgobe <dr94406@naver.com>
2025-02-11 20:14:46 +0900

    8. 테스트를 위해 일부 변경.

--- src/test/java/com/gyobongbookstore/common/api/RegisterBookApi.java
+++ src/test/java/com/gyobongbookstore/common/api/RegisterBookApi.java
@@ -1,7 +1,9 @@
 package com.gyobongbookstore.common.api;
 
 import com.gyobongbookstore.book.controller.dto.request.RegisterBookRequest;
+import com.gyobongbookstore.book.domain.enumeration.BookCondition;
 import com.gyobongbookstore.book.domain.enumeration.Category;
+import com.gyobongbookstore.book.domain.enumeration.RentalStatus;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.springframework.http.HttpStatus;
@@ -12,11 +14,23 @@ public class RegisterBookApi {
 
     private String author = "author";
     private String title = "title";
+    private BookCondition bookCondition = BookCondition.NORMAL;
+    private RentalStatus rentalStatus = RentalStatus.AVAILABLE;
     private Set<Category> categories = Set.of(Category.HUMANITIES, Category.ECONOMICSMANAGEMENT);
 
-    public void request () {
+    public RegisterBookApi rentalStatus(final RentalStatus rentalStatus) {
+        this.rentalStatus = rentalStatus;
+        return this;
+    }
 
-        RegisterBookRequest request = new RegisterBookRequest(author, title, categories);
+    public RegisterBookApi bookCondition(final BookCondition bookCondition) {
+        this.bookCondition = bookCondition;
+        return this;
+    }
+
+    public void request() {
+
+        RegisterBookRequest request = new RegisterBookRequest(author, title, bookCondition, rentalStatus, categories);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
